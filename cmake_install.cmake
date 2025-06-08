1bc643adb6022c36ff157dd36a5d5ee6bd43536e
Urek <Urek00@abv.bg>
2025-06-08 20:55:37 +0300

    I don't even know what am doing

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/vladk/Desktop/uploadUni/spaceshit2.0/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/vladk/Desktop/spaceshit2_0/spaceshit2_0/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
